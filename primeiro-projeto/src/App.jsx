e2c32918cff0f5513c2a9a70cdc188f391cc831f
MatAndrade01 <ltbmatheus141012@gmail.com>
2024-12-13 02:12:52 -0300

    Finalizando primeiro projeto!

--- primeiro-projeto/src/App.jsx
+++ primeiro-projeto/src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Navbar } from "./components/Navbar/Navbar";
-import { Counter } from "./components/Counter/Counter";
-// import { Article } from "./components/Article/Article";
+// import { Counter } from "./components/Counter/Counter";
+import { Article } from "./components/Article/Article";
 
 import './assets/styles/app.css';
 
-// import dashboard from "./assets/images/dashboard.png";
-// import vibrante from "./assets/images/Vibrant Portraits of 2020.png";
-// import day36 from "./assets/images/36 Days of Malayalam type.png";
+import dashboard from "./assets/images/dashboard.png";
+import vibrante from "./assets/images/Vibrant Portraits of 2020.png";
+import day36 from "./assets/images/36 Days of Malayalam type.png";
 
 // Componente em classe é uma classe que hera a classe do Component do React, e retorna HTML dentro do método render
 
@@ -19,34 +19,34 @@ class App extends React.Component {
       <>
       <Navbar />
 
-      <Counter />
+      {/* <Counter /> */}
   
-      {/* <section id="articles">
-      <Article 
-        title="Designing Dashboards" 
-        provider="Nasa" 
-        description ="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus culpa consequuntur perferendis, voluptatum illum est quis vel a molestiae ducimus, maxime, ullam id voluptas assumenda dignissimos? Officiis est numquam dolor?"
-        thubnail= {dashboard}
-      />
-      <Article 
-        title="Vibrant Portraits of 2020"
-        provider = "SpaceNews"
-        description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus culpa consequuntur perferendis, voluptatum illum est quis vel a molestiae ducimus, maxime, ullam id voluptas assumenda dignissimos? Officiis est numquam dolor?"
-        thubnail = {vibrante}
-      /> 
-      <Article 
-        title="36 Days of Malayalam type"
-        provider ="Spacefligth Now"
-        description ="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus culpa consequuntur perferendis, voluptatum illum est quis vel a molestiae ducimus, maxime, ullam id voluptas assumenda dignissimos? Officiis est numquam dolor?"
-        thubnail = {day36}
-      />
-      <Article 
-        title="Designing Dashboards"
-        provider="Nasa"
-        description ="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus culpa consequuntur perferendis, voluptatum illum est quis vel a molestiae ducimus, maxime, ullam id voluptas assumenda dignissimos? Officiis est numquam dolor?"
-        thubnail = {dashboard}
-      />
-      </section> */}
+      <section id="articles"> 
+        <Article 
+          title="Designing Dashboards" 
+          provider="Nasa" 
+          description ="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus culpa consequuntur perferendis, voluptatum illum est quis vel a molestiae ducimus, maxime, ullam id voluptas assumenda dignissimos? Officiis est numquam dolor?"
+          thubnail= {dashboard}
+        />
+        <Article 
+          title="Vibrant Portraits of 2020"
+          provider = "SpaceNews"
+          description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus culpa consequuntur perferendis, voluptatum illum est quis vel a molestiae ducimus, maxime, ullam id voluptas assumenda dignissimos? Officiis est numquam dolor?"
+          thubnail = {vibrante}
+        /> 
+        <Article 
+          title="36 Days of Malayalam type"
+          provider ="Spacefligth Now"
+          description ="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus culpa consequuntur perferendis, voluptatum illum est quis vel a molestiae ducimus, maxime, ullam id voluptas assumenda dignissimos? Officiis est numquam dolor?"
+          thubnail = {day36}
+        />
+        <Article 
+          title="Designing Dashboards"
+          provider="Nasa"
+          description ="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus culpa consequuntur perferendis, voluptatum illum est quis vel a molestiae ducimus, maxime, ullam id voluptas assumenda dignissimos? Officiis est numquam dolor?"
+          thubnail = {dashboard}
+        />
+      </section>
 
       </>
     )
